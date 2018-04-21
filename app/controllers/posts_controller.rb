3b63440d83ed4a3abf61212d3c3bcab0a33aac9e
kikuchi <kikuchi.3989@icloud.com>
2018-04-21 18:47:25 +0900

    markdown test

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,33 @@
 class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def new
+    @post = Post.new()
+  end
+
+  def create
+    @post = Post.new(content: params[:content])
+    if @post.save
+      flash[:notice] = "おっけー"
+      render("posts/new")
+    else
+      flash[:notice] = "だめー"
+      render("posts/new")
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get 'home/top'
-
+  get 'posts/' => 'posts#index'
+  get 'posts/new' => 'posts#new'
+  post 'posts/create' => 'posts#create'
+  get 'posts/:id' => 'posts#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
