fe561ebad2ac7d5a477d400965343526fc5d9ef6
kikuchi <kikuchi.3989@icloud.com>
2018-04-21 12:15:37 +0900

    modify

---
+++ app/assets/components/HelloList.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+const HelloList = ({}) => {
+  return (
+    <ul>
+      <li>hello, デザイン</li>
+      <li>hello, デザイン</li>
+      <li>hello, デザイン</li>
+      <li>hello, デザイン</li>
+      <li>hello, デザイン</li>
+    </ul>
+  );
+}
+
+export default HelloList
--- app/assets/components/HelloWorld.js
+++ app/assets/components/HelloWorld.js
@@ -1,16 +1,13 @@
 import React from "react"
-import PropTypes from "prop-types"
-class HelloWorld extends React.Component {
-  render () {
-    return (
-      <React.Fragment>
-        Greeting: {this.props.greeting}
-      </React.Fragment>
-    );
-  }
+import HelloList from "./HelloList"
+
+const HelloWorld = props => {
+  return (
+    <div>
+      <h3>hello, {props.data}</h3>
+      <HelloList />
+    </div>
+  );
 }
 
-HelloWorld.propTypes = {
-  greeting: PropTypes.string
-};
 export default HelloWorld
