6efe44933823a4c96b65278b4292d6a5e9ef79d2
kikuchi5555 <tetsuro.kikuchi@camp-fire.jp>
2018-04-24 14:18:09 +0900

    fix user_type

--- db/migrate/20180424051242_add_user_type_to_users.rb
+++ db/migrate/20180424051242_add_user_type_to_users.rb
@@ -1,5 +1,5 @@
 class AddUserTypeToUsers < ActiveRecord::Migration[5.1]
   def change
-    add_column :users, :user_type, :boolean
+    add_column :users, :user_type, :boolean, default: false, null: false
   end
 end
