6596030f876784e4c5af18f5c87a47a523e775eb
kikuchi5555 <tetsuro.kikuchi@camp-fire.jp>
2018-04-26 19:19:45 +0900

    add paging

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
   validates :content, {presence: true}
   validates :title, {presence: true}
   validates :tag, {presence: true}
+
+  def previous
+    Post.where('created_at <= ? and id < ? and post_status = ?', self.created_at, self.id, 'opened').order(id: :desc).first
+  end
+
+  def next
+    Post.where('created_at >= ? and id > ? and post_status = ?', self.created_at, self.id, 'opened').order(id: :asc).first
+  end
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -3,4 +3,12 @@ class Work < ApplicationRecord
   validates :category, {presence: true}
   validates :slug, {presence: true}
   validates :image_length, {presence: true}
+
+  def previous
+    Work.where('created_at <= ? and id < ?', self.created_at, self.id).order(id: :desc).first
+  end
+
+  def next
+    Work.where('created_at >= ? and id > ?', self.created_at, self.id).order(id: :asc).first
+  end
 end
