773ab874f2a62a23141a0db6af0a668b0df64d95
kikuchi5555 <tetsuro.kikuchi@camp-fire.jp>
2018-04-27 13:40:32 +0900

    add error pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,18 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :set_current_user
 
+  rescue_from ActiveRecord::RecordNotFound, with: :render_404
+  rescue_from ActionController::RoutingError, with: :render_404
+  rescue_from Exception, with: :render_500
+
+  def render_404
+    render template: 'errors/error_404', status: 404, layout: 'application', content_type: 'text/html'
+  end
+
+  def render_500
+    render template: 'errors/error_500', status: 500, layout: 'application', content_type: 'text/html'
+  end
+
   def set_current_user
     @current_user = User.find_by(id: session[:user_id])
   end
