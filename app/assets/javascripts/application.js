8d2177ec35acddb312033ec0fb6f1a9d6100ee03
kikuchi <kikuchi.3989@icloud.com>
2018-04-29 19:00:51 +0900

    add topSctoll

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,34 +1,66 @@
 document.addEventListener("DOMContentLoaded", function(){
 
   // mobile_menu
-  var menu = document.getElementById("menu");
-  var menuBtn = document.getElementById("menu-open");
-  var isActive = false;
-  menuBtn.addEventListener("click", changeActive, false);
+  var menuToggle = function(){
+    var menu = document.getElementById("menu");
+    var menuBtn = document.getElementById("menu-open");
+    var isActive = false;
+    menuBtn.addEventListener("click", changeActive, false);
 
-  function changeActive(){
-    if (isActive) {
-      menu.classList.remove('is-active');
-    } else {
-      menu.classList.add('is-active');
+    function changeActive(){
+      if (isActive) {
+        menu.classList.remove('is-active');
+      } else {
+        menu.classList.add('is-active');
+      }
+      isActive = !isActive;
     }
-    isActive = !isActive;
   }
+
+  menuToggle();
 
   // notify_bar
-  var notifyBar = document.getElementById("notify-bar");
-  var notifyBarDelay = 2000
-  if (notifyBar) {
-    notifyBarSlideIn();
-    setInterval(notifyBarSlideOut, notifyBarDelay);
+  var notifyBarSlide = function(){
+    var notifyBar = document.getElementById("notify-bar");
+    var notifyBarDelay = 2000
+    if (notifyBar) {
+      notifyBarSlideIn();
+      setInterval(notifyBarSlideOut, notifyBarDelay);
+    }
+    function notifyBarSlideIn() {
+      notifyBar.classList.add('notify-slide-in');
+    }
+    function notifyBarSlideOut() {
+      notifyBar.classList.remove('notify-slide-in');
+      notifyBar.classList.add('notify-slide-out');
+    }
   }
-  function notifyBarSlideIn() {
-    notifyBar.classList.add('notify-slide-in');
+
+  notifyBarSlide();
+
+  //toTopButton
+  var scrollTop = function(){
+    var toTopButton = document.getElementById("to-top-btn");
+    var duration = 100;
+    if(toTopButton) {
+      toTopButton.addEventListener("click", function(){
+        var currentY = window.pageYOffset;
+        var step = duration/currentY > 1 ? 10 : 100;
+        var timeStep = duration/currentY * step;
+        var intervalID = setInterval(scrollUp, timeStep);
+        function scrollUp(){
+          currentY = window.pageYOffset;
+          if(currentY === 0) {
+            clearInterval(intervalID);
+          } else {
+            scrollBy( 0, -step );
+          }
+        }
+      }, false);
+    }
   }
-  function notifyBarSlideOut() {
-    notifyBar.classList.remove('notify-slide-in');
-    notifyBar.classList.add('notify-slide-out');
-  }
+
+  scrollTop();
 
   // fixed-login-form
   // var loginForm = document.getElementById("login-form");
