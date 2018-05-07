2f35612a9734cc7f444f640c2ddadc7832508f31
kikuchi5555 <tetsuro.kikuchi@camp-fire.jp>
2018-05-07 17:05:14 +0900

    set react-dropzone

--- app/assets/packs/app.js
+++ app/assets/packs/app.js
@@ -3,9 +3,9 @@ import 'javascripts/application'
 import 'stylesheets/application'
 
 // Support component names relative to this directory:
-// var componentRequireContext = require.context("components", true)
-// var ReactRailsUJS = require("react_ujs")
-// ReactRailsUJS.useContext(componentRequireContext)
+var componentRequireContext = require.context("components", true)
+var ReactRailsUJS = require("react_ujs")
+ReactRailsUJS.useContext(componentRequireContext)
 
 import Rails from 'rails-ujs';
 Rails.start();
