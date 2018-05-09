43ebc6827477dd7cd9f885fc3212df28d38bc706
kikuchi <kikuchi.3989@icloud.com>
2018-05-09 16:06:07 +0900

    fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -96,8 +96,6 @@ document.addEventListener("DOMContentLoaded", function(){
   var topAnimation = function(){
     var profileTitle = document.getElementById('profile-title');
     var profileBox = document.getElementById('profile-box');
-    var menu = document.getElementById('menu-wrap');
-    var logo = document.getElementById('logo');
 
     if (!profileTitle) return;
     var addText1 = () => {
@@ -217,8 +215,6 @@ document.addEventListener("DOMContentLoaded", function(){
     var removeText7 = () => {
       profileTitle.innerHTML = "";
     }
-    menu.classList.add('is-hidden');
-    logo.classList.add('is-hidden');
     setTimeout(addText1, 1000);
     setTimeout(addText2, 1200);
     setTimeout(addText3, 1700);
@@ -254,8 +250,6 @@ document.addEventListener("DOMContentLoaded", function(){
     setTimeout(() => {profileTitle.classList.remove('is-focus')}, 9800);
     setTimeout(() => {profileTitle.classList.add('is-active')}, 10000);
     setTimeout(() => {profileBox.classList.add('is-active')}, 11500);
-    setTimeout(() => {menu.classList.remove('is-hidden')}, 11500);
-    setTimeout(() => {logo.classList.remove('is-hidden')}, 11500);
   }
 
   topAnimation();
