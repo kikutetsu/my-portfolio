fa67decd985a7952845b214eda3cd98e2818154f
kikuchi <kikuchi.3989@icloud.com>
2018-09-15 19:58:10 +0900

    fix

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   before_action :authenticate_user
 
   def about
-    @posts = Post.where(post_status: "opened").order('id desc').limit(6)
+    # @posts = Post.where(post_status: "opened").order('id desc').limit(6)
   end
 end
