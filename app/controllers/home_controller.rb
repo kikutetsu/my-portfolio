aba1f4fe33bdb0c76c4c8c5d14a27c8941a7b428
kikuchi <kikuchi.3989@icloud.com>
2018-09-15 19:06:12 +0900

    fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   before_action :authenticate_user
   def top
-    @works = Work.all.limit(6)
+    @works = Work.all.order('id desc').limit(6)
   end
 end
